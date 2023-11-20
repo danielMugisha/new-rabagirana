a104dde67226f996eb256bdca3c24ceb9cd95e8d
danielMugisha <dmugisha189@gmail.com>
2023-11-20 17:08:41 +0200

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,163 @@
+const stories = [
+	{
+		id: 1,
+		title: "Umusozi w'Ubumwe",
+		summary: `Rusheshe community was known for disunity, disputes, lack of interest in government programs...`,
+		content: `Rusheshe community was known for disunity, disputes, lack of interest in government programs such as medical insurance for all, every child in school and other good government targets. From January 2021, Rabagirana Ministry adopted Rusheshe as a model community where Rwandans and other countries will come and experience authentic healing, forgiveness and reconciliation. In our first meeting, Isaiah 11 was our theme with through pillars: God is the true source of lasting and reconciliation The church should be an agent of God’s restoration in the community When the Kingdom of God comes, former enemies should be working together towards a peaceful and prosperous future. Today, Rusheshe has been renamed ‘ Umusozi w’Ubumwe’ or the Unity Mountain. Unity groups made of all social strata meet to share their pain and joys, visit each other, learn skills together and do small businesses to tackle poverty. Rachel, the leader of the committee says: ‘God has taken away the shame over our community. I am now proud to belong here.’ One local government leader said ‘I considered it a blessing when I was sent to work elsewhere from this extremely difficult community.’ Today as I listen to perpetrators and survivors, to exiles and those who lived here, I am moved.’ One guest from the US: ‘as an African American, I came in Rwanda with doubt. I could not believe true reconciliation is possible. I am amazed at Umusozi w’Ubumwe to see how Rwanda are ‘re-humanizing’ one another, including people who committed terrible atrocities.’`,
+		narrator: "Rabagirana",
+		image: "assets/images/stories/Umusozi.jpg",
+	},
+	{
+		id: 2,
+		title: "Keza’s story…",
+		summary: `The month was July, the day was a Saturday, and the time was half-past midday. A joyous Keza Angelique...`,
+		content: `The month was July, the day was a Saturday, and the time was half-past midday. A joyous Keza Angelique, a single mother of three, is humming to a hymn from the radio as she warms ‘isombe’ (cassava leaves) for lunch, using her newly purchased gas cooker. With a green colourful ‘kitenge’ (wrapper) around her waist and a bandana of the same colour covering her head, she turns to us and asks, “do you want to know the secret ingredient to my tasty isombe?” “Love!” my colleague immediately yelled. “No. Love doesn’t make my isombe sweet. What’s wrong with you?” She replied playfully, making us all burst into laughter. After she was done warming the isombe, she got ready to prepare ‘ugali’ (sorghum flour). She sprinkled the flour into the boiling water while briskly stirring it with a cooking stick to make it thicker. In a very short time, the ugali was ready and it was time to eat. As we sat by the dining table to devour the tasty meal that lay helplessly before us, Keza signalled that she wanted to say something. My colleague and I immediately had one of those telepathic communications, questioning Keza’s poor timing. “One doesn’t give speeches before the meal…speeches usually take place after the meal!" he seemed to say. "Her house, her rules I guess," I signalled back.  So, we sat quietly and listened to her.
+
+		“Thank you, Rabagirana Ministries for the gas cooker. Cooking over the traditional three-stone open fire was a health hazard to me. It used to make me cough a lot and the smoke would hurt my eyes too... giving me teary eyes with a running nose.  But this gas produces no smoke and since I started using it, I do not cough anymore.”
+		
+		Keza is one of many community members positively impacted by the creation care program of Rabagirana Ministries. Earlier last month we partnered with some local cooperatives to purchase gas cookers, and build wood-saving, earth-block stoves popularly known as the energy-saving stoves for the members of the community. As compared to the widely used traditional three-stone open hearth, these alternatives are already transforming the way people cook in rural communities.`,
+		narrator: "Rabagirana",
+		image: "assets/images/stories/Gas.jpeg",
+	},
+	{
+		id: 3,
+		title: "Ubumwe football",
+		summary: `Ubumwe football is a team of single mothers that came together after attending healing heart transforming...`,
+		content: `Ubumwe football is a team of single mothers that came together after attending healing heart transforming nation workshop (HHTN). it started in 2021 February and they are 20 members.
+		These women were hopeless of the future and wounded because they were rejected by their families but after HHTN workshop, they discovered that GOD always has hope even if we don’t, then they decided to start Football team, they saved some little amount of money on weekly basis, Right now they have an income generating activity of livestock of pigs in a program called Pig Per Player Project (4P's). These women normally survive on small business of selling vegetables (street vendors) 4 of them have had chance to get legally married as a result of healing from past experiences in the football team.
+		UBUMWE FC has been able to spread the message since it started, the team has played with many teams such as, COERSO tourist Students from SWTZLAND, Eleven football Academy, Africa Rise Team , Forum of reconciliation, Gatenga women football team in Umurenge Kagame Cup, and many more. these different activities helped them to developed in social and economic welfare.
+		The vision is to see their team being the channel of spreading the message of peace in the community, it is their desire to become competent at national and international level and holistic transformation to their life.`,
+		narrator: "Rabagirana",
+		image: "assets/images/stories/ubumwe-football.jpg",
+	},
+	{
+		id: 4,
+		title: "Mutesi's story",
+		summary: `Mutesi is a genocide survivor who is the only survivor in her family, after the genocide she...`,
+		content: `Mutesi is a genocide survivor who is the only survivor in her family, after the genocide she lived a hopeless and miserable life, she lived a lonely life and trusted no one on both Hutu side and Tutsi side. she later on joined our workshop and found freedom,dealt with her pain. Mukeshimana is a wife of an Ex perpetrator, in otherwise the husband killed many people during the Genocide. her husband was imprisoned for 28 years. she lived in denial, guilt and dispair. after going through the healing workshop she felt peace and even received Jesus Christ in the process. now these two ladies joined many more with the same stories through Rabagirana and started a cooperative (Ubumwe crafts) these ladies were trained to make crafts which includes jewellery, baskets, different african print decorations, cultural objects with a meaning. they are now able to generate some little income and are able to provide for their families but most importantly while working on the crafts together, they get an opportunity to share life and experiences as they counsel each other.`,
+		narrator: "Rabagirana",
+		image: "assets/images/stories/mutesi.jpg",
+	},
+	{
+		id: 5,
+		title: "Alex's story",
+		summary: `My name is Mukunzi Alex, and I came very close to committing suicide last month. Here’s my story...`,
+		content: `My name is Mukunzi, and I came very close to committing suicide last month. Here’s my story:
+		My father is a typical alpha male who does not tolerate weakness of any kind. I was not allowed to talk about my feelings. "Talking about your feelings is for women," he would say. He taught me to bottle up my feelings from when I was a little boy. I reached my breaking point when I went through a near-death experience after getting involved in an accident, but I still wasn't permitted to talk about it. That was when I decided that it would be better to be dead than live such a miserable life.
+		It was shortly after contemplating suicide that I bumped into a friend who told me about the counseling sessions at Rabagirana Ministries on Tuesdays and Thursdays. I was reluctant at first because, in the back of my mind, I knew if my father found out, he would be very disappointed in me. Nonetheless, my friend, whose father is also a soldier, insisted, and I decided to give it a try. That one counseling session was life-transforming. I was allowed to speak about everything without judgment. The counselor was both empathetic and sympathetic, something I have never experienced from my father. Since then, I have been regulary attending counseling.
+		 Thank you, RM, for without you, I would be six feet deep.`,
+		narrator: "Alex Mukunzi",
+		image: "assets/images/stories/mukunzi-alex.jpg",
+	},
+	{
+		id: 6,
+		title: "The Healing Saloon",
+		summary: `Meet Mukeshimana Claudine; she is a wife, a mother of four, a hairdresser, and of recent, a healing`,
+		content: `Meet Mukeshimana Claudine; she is a wife, a mother of four, a hairdresser, and of recent, a healing and
+		reconciliation practitioner! Rabagirana Ministries (RM) got to know about Claudine a few months ago when she
+		enrolled for our annual Vocational training school. Before taking the hairdressing class, Claudine narrates how
+		miserable life was; &quot;I was unemployed for as long as I can remember&quot; she begins. &quot;My husband and I used to
+		struggle daily to provide for our kids their basic needs. It was a very tough time. This however, is now all in the
+		past!&quot; she concludes with a smile.
+		After her completion of the course, Claudine didn&#39;t take long before she landed her first job. She now works at
+		Magic Salon, which is also known by the locals as the &quot;healing salon.&quot; The nickname &quot;healing salon&quot; came from
+		the fact that people actually felt better after spending time in that salon and Claudine was the party behind that.
+		After going through the healing and reconciliation workshop, Claudine recalls experiencing a deep healing of all
+		her wounds and she vowed to use whatever platform God was going to give her to share her experience. So,
+		when the opportunity presented itself, she took it. She would offer compassionate hearing to her clients and
+		allow them to share their hurts and release their tears. Many women, old and young alike, have now received the
+		healing message, and for majority of them, it changed their outlook on life.
+		It is now very clear that most Rwandans are not asking if healing is possible but where can they find it? We
+		want to encourage every church, school, business, and even homes to be used as a &#39;healing space!&#39;`,
+		narrator: "Rabagirana",
+		image: "assets/images/stories/Healing-saloon.jpg",
+	},
+];
+
+window.onload = () => {
+    var hero = document.getElementById("hero");
+    var video = document.getElementById("myVideo");
+    var btn = document.getElementById("play-btn");
+    hero.addEventListener("click", function () {
+    if (video.paused){
+        video.play();
+        video.muted = false;
+        btn.style.display = "none";
+    }else{
+        btn.style.display = "block";
+        video.pause();
+    }
+    });
+    getElements();
+};
+
+ const getElements = () => {
+ 		buildStories();
+ };
+
+
+
+const buildStories = () => {
+	var el = document.getElementById("stories");
+	el.innerHTML = "";
+	stories.forEach((story) => {
+		el.innerHTML += `<div class="carousel-item col-12 col-lg-6 col-xl-4 mr-60">
+        <article
+            class="lqd-pf-item lqd-pf-item-style-3 lqd-pf-overlay-bg-scale lqd-pf-content-v pf-details-h-str"
+        >
+            <div class="lqd-pf-item-inner">
+                <div
+                    class="lqd-pf-img overflow-hidden rounded-6 relative mb-2em"
+                >
+                    <figure>
+                        <figure class="lqd-overlay flex">
+                            <img
+                                width="640"
+                                height="600"
+                                src=${story.image}
+                                class="w-full h-full objfit-cover objfit-center"
+                                alt="case study"
+                            />
+                        </figure>
+                    </figure>
+                    <span
+                        class="lqd-pf-overlay-bg lqd-overlay flex items-center justify-center bg-transparent text-white"
+                        style="
+                            background-image: linear-gradient(
+                                180deg,
+                                rgb(243, 194, 72, 0.3) 0%,
+								rgba(97, 151, 95, 0.3) 100%
+                            );
+                        "
+                        >
+						<p
+												class="ld-fh-element inline-block relative mb-0/5em text-20 leading-1/7em"
+											style="margin: 16px">
+												${story.summary}
+											</p>
+						</span>
+                </div>
+                <div class="lqd-pf-details">
+                    <h2 class="lqd-pf-title mt-0 mb-1 h5">
+                        ${story.title}
+                    </h2>
+                    <ul
+                        class="reset-ul inline-nav lqd-pf-cat inline-flex relative z-2"
+                    >
+                        <li>
+                            <a href="#" class="leading-1/4em"
+                                >${story.narrator}</a
+                            >
+                        </li>
+                    </ul>
+                </div>
+                <a
+                    href="/story.html?id=${story.id}"
+                    class="lqd-overlay flex lqd-pf-overlay-link leading-1/4em "
+                    data-fresco-group="case-studies"
+                ></a>
+            </div>
+        </article>
+    </div>`;
+	});
+};
